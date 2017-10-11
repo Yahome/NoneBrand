1a3b0cc55dc2d113254143739b2edeef8e5b792c
Yahome <cyh19910403@sina.com>
2017-10-11 17:59:44 +0800

    项目提交

---
+++ src/main/java/com/ijnkj/CapShopApplication.java
@@ -0,0 +1,12 @@
+package com.ijnkj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapShopApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CapShopApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ijnkj/Controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.ijnkj.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping(value="/")
+	@ResponseBody
+	public String index() {
+		return "Welcome Spring boot";
+	}
+}
